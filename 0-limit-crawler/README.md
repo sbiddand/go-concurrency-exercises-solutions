e0f965deedae9a2a2bb94d06570c19ede39835d5
Long Hoang <long@mindworker.de>
2016-04-11 17:25:05 +0800

    Add solution checking for limit crawler exercise

--- 0-limit-crawler/README.md
+++ 0-limit-crawler/README.md
@@ -4,3 +4,23 @@ Given is a crawler (modified from the Go tour), which would pull pages excessive
 
  - the crawler cannot crawl more than 2 pages per second
  - without losing it's concurrency
+
+# Test your solution
+
+Use `go test` to verify if your solution is correct.
+
+Correct solution:
+```
+PASS
+ok      github.com/mindworker/go-concurrency-exercises/0-limit-crawler  13.009s
+```
+
+Incorrect solution:
+```
+--- FAIL: TestMain (7.80s)
+        main_test.go:18: There exists a two crawls who were executed less than 1 sec apart.
+	        main_test.go:19: Solution is incorrect.
+		FAIL
+		exit status 1
+		FAIL    github.com/mindworker/go-concurrency-exercises/0-limit-crawler  7.808s
+```
--- 0-limit-crawler/check_test.go
+++ 0-limit-crawler/check_test.go
@@ -1,14 +1,18 @@
+//////////////////////////////////////////////////////////////////////
+//
+// DO NOT EDIT THIS PART
+// Your task is to edit `main.go`
+//
+
 package main
 
 import (
-	"sync"
 	"testing"
 	"time"
 )
 
 func TestMain(t *testing.T) {
 	fetchSig := fetchSignalInstance()
-	done := make(chan bool)
 
 	start := time.Now()
 	go func() {
@@ -16,21 +20,15 @@ func TestMain(t *testing.T) {
 			switch {
 			case <-fetchSig:
 				// Check if signal arrived earlier than a second (with error margin)
-				if time.Since(start).Nanoseconds() < 990000000 {
+				if time.Now().Sub(start).Nanoseconds() < 990000000 {
 					t.Log("There exists a two crawls who were executed less than 1 sec apart.")
 					t.Log("Solution is incorrect.")
 					t.FailNow()
 				}
 				start = time.Now()
-			case <-done:
-				break
 			}
 		}
 	}()
 
-	var wg sync.WaitGroup
-	wg.Add(1)
-	Crawl("http://golang.org/", 4, &wg)
-	wg.Wait()
-	done <- true
+	main()
 }
